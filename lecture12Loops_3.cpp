146e0365a342a20a71de3ab92b4f750904e777da
Developer9985 <94001072+Developer9985@users.noreply.github.com>
2024-06-19 17:18:30 +0530

    Add files via upload

---
+++ lecture12Loops_3.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // Demonstration of do while loop
+
+    int k = 0;
+    do
+    {
+        cout<< k << " "; 
+        k++;
+    } while (k < 5);
+
+
+    // Password manager program 
+
+    
+    
+
+
+
+
+}
